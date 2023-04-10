c4fdfb98b6d872d731b511d03e0f957ff1e69f86
Adrián López <adrianlomu2004@gmail.com>
2023-04-10 12:00:41 +0200

    (build): implementar maven

---
+++ kata_furance_dip/src/main/java/edu/craptocraft/kata_furance_dip/App.java
@@ -0,0 +1,28 @@
+package edu.craptocraft.kata_furance_dip;
+
+public class App {
+    public static void main( String[] args )
+    {
+    	final double minTemp = 15.0;
+        final double maxTemp = 21.0;
+        
+        RoomTemperature temperature = RoomTemperature.getInstance();
+        temperature.setTemperature(17);
+        Heater heater = new GasHeater();
+        Thermometer thermometer = new RemoteCommandSensor();
+        
+        Regulator regulator = new Regulator();
+        
+        System.out.println( "Arrancando..." );
+        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+        
+        temperature = RoomTemperature.getInstance();
+        Jedi yoda = new Jedi();
+        System.out.println( "\nArrancando a Yoda: " );
+        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
+        yoda.speak();
+
+        Heater luke = new Jedi();
+        // luke.speak(); // ups, The method speak() is undefined for the type HeaterJava
+    }
+}
